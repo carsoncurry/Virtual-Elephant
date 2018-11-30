7fb73420a65b2f6c878244e02f6e80300da89eb1
Carson Curry <carsoncurry52@gmail.com>
2018-11-29 21:35:24 -0600

    force: true

--- models/example.js
+++ models/example.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT,
-    gifter: DataTypes.TEXT,
+    gifter: DataTypes.STRING,
     gift: DataTypes.TEXT
   });
   return Example;
