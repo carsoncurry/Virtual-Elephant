b5b26fd9cbdf818662de03102a0a807bd346b2c4
Carson Curry <carsoncurry52@gmail.com>
2018-12-01 10:52:04 -0600

    copy

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
-var bodyParser = require("body-parser");
 var socket = require('socket.io');
-var xoauth2 = require('xoauth2');
 
 var db = require("./models");
-
-var path = require('path');
-
-
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,15 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-//body parser
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-//nodemailer idex
-app.get("api/friend", function (req, res) {
-  res.render("friend");
-});
 // Handlebars
 app.engine(
   "handlebars",
@@ -41,7 +26,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -50,11 +35,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
 db.sequelize.sync(syncOptions).then(function() {
   const server = app.listen(PORT, function() {
-
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
